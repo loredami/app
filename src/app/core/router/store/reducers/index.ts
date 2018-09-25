32c468d2098d20a18e7ee67a0536e20cdc0d6d01
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-09-26 00:57:23 +0200

    refactor(core): router

--- src/app/core/router/store/reducers/index.ts
+++ src/app/core/router/store/reducers/index.ts
@@ -1,8 +1,11 @@
 import * as fromRouter from '@ngrx/router-store';
 import { RouterStateUrl } from './router.serializer';
+import * as fromRoot from 'core/store/reducers';
 
 export * from './router.serializer'
 
-export interface State {
-  router: fromRouter.RouterReducerState<RouterStateUrl>;
+export type RouterState = fromRouter.RouterReducerState<RouterStateUrl>;
+
+export interface State extends fromRoot.State {
+  router: RouterState;
 }
--- src/app/core/store/reducers/index.ts
+++ src/app/core/store/reducers/index.ts
@@ -6,14 +6,19 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../../../environments/environment';
+import { AuthState } from 'core/auth';
+import { RouterState } from 'core/router';
+
+export interface AppState {
+  auth: AuthState,
+  router: RouterState
+}
 
 export interface State {
 
 }
 
-export const reducers: ActionReducerMap<State> = {
-
-};
+export const reducers: ActionReducerMap<State> = {};
 
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
